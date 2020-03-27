b5aabb867d3b3b95a7b1a40c64391a8f20abc393
Yannik Höll <labruzzler@gmail.com>
2020-03-27 15:54:03 +0100

    fixed bug with position

--- js/parser.js
+++ js/parser.js
@@ -9,12 +9,11 @@ let json = {
 
 const circuits = {};
 
-function pushComponent(name, inputs, outputs, label, bits=1, extraJson={}, pos=undefined) {
+function pushComponent(name, inputs, outputs, label, bits=1, extraJson={}) {
     components.push({
         celltype: name,
         inputs: inputs,
         outputs: outputs,
-        pos: pos,
         bits: bits,
         label: label,
         extraJson: extraJson
@@ -23,120 +22,120 @@ function pushComponent(name, inputs, outputs, label, bits=1, extraJson={}, pos=u
 
 // standard logic gates
 function AND(inputs, outputs, label, bits=1, pos=undefined) {
-    pushComponent("$and", inputs, outputs, label, bits, pos);
+    pushComponent("$and", inputs, outputs, label, bits, {}, {position: pos});
 }
 
 function NAND(inputs, outputs, label, bits=1, pos=undefined) {
-    pushComponent("$nand", inputs, outputs, label, bits, pos);
+    pushComponent("$nand", inputs, outputs, label, bits, {position: pos});
 }
 
 function OR(inputs, outputs, label, bits=1, pos=undefined) {
-    pushComponent("$or", inputs, outputs, label, bits, pos);
+    pushComponent("$or", inputs, outputs, label, bits, {position: pos});
 }
 
 function NOR(inputs, outputs, label, bits=1, pos=undefined) {
-    pushComponent("$nor", inputs, outputs, label, bits, pos);
+    pushComponent("$nor", inputs, outputs, label, bits, {position: pos});
 }
 
 function XOR(inputs, outputs, label, bits=1, pos=undefined) {
-    pushComponent("$xor", inputs, outputs, label, bits, pos);
+    pushComponent("$xor", inputs, outputs, label, bits, {position: pos});
 }
 
 function XNOR(inputs, outputs, label, bits=1, pos=undefined) {
-    pushComponent("$xnor", inputs, outputs, label, bits, pos);
+    pushComponent("$xnor", inputs, outputs, label, bits, {position: pos});
 }
 // --------------
 
 // Reducing gates
 function ReducingAND(input, output, label, bits, pos=undefined) {
-    pushComponent("$reduce_and", [input], [output], label, bits, pos);
+    pushComponent("$reduce_and", [input], [output], label, bits, {position: pos});
 }
 
 function ReducingNAND(input, output, label, bits, pos=undefined) {
-    pushComponent("$reduce_nand", [input], [output], label, bits, pos);
+    pushComponent("$reduce_nand", [input], [output], label, bits, {position: pos});
 }
 
 function ReducingOR(input, output, label, bits, pos=undefined) {
-    pushComponent("$reduce_or", [input], [output], label, bits, pos);
+    pushComponent("$reduce_or", [input], [output], label, bits, {position: pos});
 }
 
 function ReducingNOR(input, output, label, bits, pos=undefined) {
-    pushComponent("$reduce_nor", [input], [output], label, bits, pos);
+    pushComponent("$reduce_nor", [input], [output], label, bits, {position: pos});
 }
 
 function ReducingXOR(input, output, label, bits, pos=undefined) {
-    pushComponent("$reduce_xor", [input], [output], label, bits, pos);
+    pushComponent("$reduce_xor", [input], [output], label, bits, {position: pos});
 }
 
 function ReducingXNOR(input, output, label, bits, pos=undefined) {
-    pushComponent("$reduce_xnor", [input], [output], label, bits, pos);
+    pushComponent("$reduce_xnor", [input], [output], label, bits, {position: pos});
 }
 
 // --------------
 
 // Constant output of binary value
 function NumberConstant(output, label, value, pos=undefined) {
-    pushComponent("$constant", [], [output], label, undefined, {constant: value}, pos);
+    pushComponent("$constant", [], [output], label, undefined, {constant: value, position: pos});
 }
 // -----------------
 
 // Unary operations
 function Negator(input, output, label, bitsIn, bitsOut, signed, pos=undefined) {
-    pushComponent("$neg", [input], [output], pos, label, {in: bitsIn, out: bitsOut}, {signed: signed}, pos);
+    pushComponent("$neg", [input], [output], pos, label, {in: bitsIn, out: bitsOut}, {signed: signed, position: pos});
 }
 // ----------------
 
 // Binary arithmetic operations
 
 function Add(inputs, output, label, bitsIn, bitsOut, signed, pos=undefined) {
-    pushComponent("$add", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}}, pos);
+    pushComponent("$add", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}, position: pos});
 }
 
 function Subtract(inputs, output, label, bitsIn, bitsOut, signed, pos=undefined) {
-    pushComponent("$sub", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}}, pos);
+    pushComponent("$sub", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}, position: pos});
 }
 
 function Multiply(inputs, output, label, bitsIn, bitsOut, signed, pos=undefined) {
-    pushComponent("$mul", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}}, pos);
+    pushComponent("$mul", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}, position: pos});
 }
 
 function Divide(inputs, output, label, bitsIn, bitsOut, signed, pos=undefined) {
-    pushComponent("$div", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}}, pos);
+    pushComponent("$div", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}, position: pos});
 }
 
 function Modulo(inputs, output, label, bitsIn, bitsOut, signed, pos=undefined) {
-    pushComponent("$mod", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}}, pos);
+    pushComponent("$mod", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}, position: pos});
 }
 
 function Pow(inputs, output, label, bitsIn, bitsOut, signed, pos=undefined) {
-    pushComponent("$pow", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}}, pos);
+    pushComponent("$pow", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut}, {signed: {in1: signed, in2: signed}, position: pos});
 }
 
 // ---------------
 
 // Comparison operations
 function Equals(inputs, output, label, bitsIn, signed, pos=undefined) {
-    pushComponent("$eq", inputs, [output], pos, label, {in1: bitsIn, in2: bitsIn}, {in1: signed, in2: signed}, pos);
+    pushComponent("$eq", inputs, [output], pos, label, {in1: bitsIn, in2: bitsIn}, {signed: {in1: signed, in2: signed}, position: pos});
 }
 
 function NotEquals(inputs, output, label, bitsIn, signed, pos=undefined) {
-    pushComponent("$ne", inputs, [output], label, {in1: bitsIn, in2: bitsIn}, {in1: signed, in2: signed}, pos);
+    pushComponent("$ne", inputs, [output], label, {in1: bitsIn, in2: bitsIn}, {signed: {in1: signed, in2: signed}, position: pos});
 }
 
 function LessThan(inputs, output, label, bitsIn, signed, pos=undefined) {
-    pushComponent("$lt", inputs, [output], label, {in1: bitsIn, in2: bitsIn}, {in1: signed, in2: signed}, pos);
+    pushComponent("$lt", inputs, [output], label, {in1: bitsIn, in2: bitsIn}, {signed: {in1: signed, in2: signed}, position: pos});
 }
 
 function LessThanOrEqual(inputs, output, label, bitsIn, signed, pos=undefined) {
-    pushComponent("$le", inputs, [output], label, {in1: bitsIn, in2: bitsIn}, {in1: signed, in2: signed}, pos);
+    pushComponent("$le", inputs, [output], label, {in1: bitsIn, in2: bitsIn}, {signed: {in1: signed, in2: signed}, position: pos});
 }
 
 function GreaterThan(inputs, output, label, bitsIn, signed, pos=undefined) {
-    pushComponent("$gt", inputs, [output], label, {in1: bitsIn, in2: bitsIn}, {signed: {in1: signed, in2: signed}}, pos);
+    pushComponent("$gt", inputs, [output], label, {in1: bitsIn, in2: bitsIn}, {signed: {in1: signed, in2: signed}, position: pos});
 }
 
 function GreaterThanOrEqual(inputs, output, label, bitsIn, signed, pos=undefined) {
-    pushComponent("$ge", inputs, [output], label, {in1: bitsIn, in2: bitsIn}, {signed: {in1: signed, in2: signed}}, pos);
+    pushComponent("$ge", inputs, [output], label, {in1: bitsIn, in2: bitsIn}, {signed: {in1: signed, in2: signed}});
 }
 
 // -----------------
@@ -144,37 +143,37 @@ function GreaterThanOrEqual(inputs, output, label, bitsIn, signed, pos=undefined
 // Binary shift operations
 function ShiftLeft(inputs, output, label, bitsIn, bitsOut, signed, pos=undefined) {
     pushComponent("$shl", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut},
-        {signed: {in1: signed, in2: signed, out: signed}, fillx: false}, pos);
+        {signed: {in1: signed, in2: signed, out: signed}, fillx: false, position: pos});
 }
 
 function ShiftRight(inputs, output, label, bitsIn, bitsOut, signed, pos=undefined) {
     pushComponent("$shr", inputs, [output], label, {in1: bitsIn, in2: bitsIn, out: bitsOut},
-        {signed: {in1: signed, in2: signed, out: signed}, fillx: false}, pos);
+        {signed: {in1: signed, in2: signed, out: signed}, fillx: false, position: pos});
 }
 
 // -----------------
 
 // Misc ------------
 function Multiplexer(inputs, selInput, output, label, bitsIn, bitsSel, pos=undefined) {
-    pushComponent("$mux", [...inputs, selInput], [output], label, {in: bitsIn, sel: bitsSel}, pos);
+    pushComponent("$mux", [...inputs, selInput], [output], label, {in: bitsIn, sel: bitsSel}, {position: pos});
 }
 // -----------------
 
 // IO -------------
 function Button(output, label, pos=undefined) {
-    pushComponent("$button", [], [output], label, pos);
+    pushComponent("$button", [], [output], label, 1, {position: pos});
 }
 
 function Lamp(input, label, pos=undefined) {
-    pushComponent("$lamp", [input], [], label, pos);
+    pushComponent("$lamp", [input], [], label, 1, {position: pos});
 }
 
 function NumberInput(output, label, bitsOut, base, pos=undefined) {
-    pushComponent("$numentry", [], [output], label, bitsOut, {numbase: base}, pos);
+    pushComponent("$numentry", [], [output], label, bitsOut, {numbase: base, position: pos});
 }
 
 function NumberOutput(input, label, bitsIn, base, pos=undefined) {
-    pushComponent("$numdisplay", [input], [], label, bitsIn, {numbase: base}, pos);
+    pushComponent("$numdisplay", [input], [], label, bitsIn, {numbase: base, position: pos});
 }
 // ---------------
 
@@ -273,7 +272,7 @@ function finalizeJSON(name) {
     }
 
     circuits[name] = json;
-    
+
     // reset state
     components = [];
     wires = [];
